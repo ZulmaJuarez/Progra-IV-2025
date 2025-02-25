b4f8bc7db8253152447a8219b00cbaba0b02e4af
Zulma <jjulyjuarezf04@gmail.com>
2025-02-24 21:12:56 -0600

    se termino el formulario de alumnos y materias con Vue.js + IndexedDB + Dexie.js

--- main.js
+++ main.js
@@ -1,61 +1,41 @@
-const {createApp} = Vue;
+const {createApp, ref} = Vue;
+const Dexie = window.Dexie,
+    db = new Dexie('db_academico');
 
-createApp({
+const app = createApp({
+    components: {
+        alumno,
+        materia,
+        buscaralumno,
+        buscarmateria
+    },
     data() {
         return {
-            alumnos: [],
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            municipio: '',
-            departamento: '',
-            telefono: '',
-            fecha_nacimiento: '',
-            sexo: '',
-        }
+            forms : {
+                alumno: {mostrar: false},
+                buscarAlumno: {mostrar: false},
+                materia: {mostrar: false},
+                buscarMateria: {mostrar: false},
+                matricula: {mostrar: false},
+            },
+        };
     },
     methods: {
-        eliminarAlumno(alumno) {
-            if (confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`)){
-                localStorage.removeItem(alumno.codigo);
-                this.listarAlumnos();
-            }
+        buscar(form, metodo) {
+            this.$refs[form][metodo]();
         },
-        verAlumno(alumno) {
-            this.codigo = alumno.codigo;
-            this.nombre = alumno.nombre;
-            this.direccion = alumno.direccion;
-            this.municipio = alumno.municipio;
-            this.departamento = alumno.departamento;
-            this.telefono = alumno.telefono;
-            this.fecha_nacimiento = alumno.fecha_nacimiento;
-            this.sexo = alumno.sexo;
+        abrirFormulario(componente) {
+            this.forms[componente].mostrar = !this.forms[componente].mostrar;
         },
-        guardarAlumno() {
-            let alumno = {
-                codigo: this.codigo,
-                nombre: this.nombre,
-                direccion: this.direccion,
-                municipio: this.municipio,
-                departamento: this.departamento,
-                telefono: this.telefono,
-                fecha_nacimiento: this.fecha_nacimiento,
-                sexo: this.sexo
-                
-            };
-            localStorage.setItem(this.codigo, JSON.stringify(alumno));
-            this.listarAlumnos();
-        },
-        listarAlumnos() {
-            this.alumnos = [];
-            for (let i = 0; i < localStorage.length; i++) {
-                let clave = localStorage.key(i),
-                    valor = localStorage.getItem(clave);
-                this.alumnos.push(JSON.parse(valor));
-            }
+        modificar(form, metodo, datos) {
+            this.$refs[form][metodo](datos);
         }
     },
     created() {
-        this.listarAlumnos();
+        db.version(1).stores({
+            alumnos: '++idAlumno, codigo, nombre, direccion, telefono, email',
+            materias: '++idMateria, codigo, nombre, uv',
+        });
     }
-}).mount('#app');
+});
+app.mount('#app');
